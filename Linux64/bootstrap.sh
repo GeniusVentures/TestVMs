a032a1bf584f059066b5f0ea0010989c2df458b8
Super Genius <ken@gnus.ai>
2022-12-16 21:03:46 -0800

    Updating to Ubunutu 22.04 to align with github actions images

--- Linux64/bootstrap.sh
+++ Linux64/bootstrap.sh
@@ -22,7 +22,21 @@ apt-get -y update
 # Development Tools
 ###
 echo -e "$CYAN--- Installing Development tools/libraries ---$NO_COLOR"
-apt-get -y install g++ llvm clang++ clang cmake
+apt-get -y install g++ llvm cmake 
+
+# setup android NDK link.
+
+cat << EOF >> /home/vagrant/.profile
+export MAKEFLAGS="-j 8"
+export CMAKE_BUILD_PARALLEL_LEVEL=8
+declare -x ANDROID_HOME="/SSDevelopment/Development/ThirdParty/Android"
+declare -x ANDROID_NDK="$ANDROID_HOME/ndk/android-ndk-r23b"
+declare -x ANDROID_NDK_HOME="$ANDROID_NDK"
+declare -x ANDROID_TOOLCHAIN="$ANDROID_NDK/toolchains/llvm/prebuilt/Linux-x86_64/bin"
+export PATH="$ANDROID_TOOLCHAIN:$PATH"
+EOF
+
+ln -s /usr/bin/python3 /usr/bin/python
 
 ###
 # open up ports
